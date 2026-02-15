50b71b0ae5cb6b785cbee06ed8e58b1daacff800
LosAngelos <19857198709@163.com>
2026-02-15 16:30:18 +0800

    modify sth in CMakeLists.txt

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,9 +1,22 @@
 
-
-#include<iostream>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XTest.h>
+#include <iostream>
+#include <vector>
+#include <jpeglib.h>
 
 int main()
 {
     std::cout << " hello world " << std::endl;
+
+    Display* display = XOpenDisplay(nullptr);
+
+    if(!display)
+    {
+        
+    }
+
+
     return 0;
 }
